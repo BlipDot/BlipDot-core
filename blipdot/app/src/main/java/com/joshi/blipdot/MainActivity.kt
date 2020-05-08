955d84479850b5ae98235e2893159cdea755348a
Creatune <rjjoshixyz@gmail.com>
2020-05-08 21:19:50 +0530

    Added animation to the dot such that it fades in and out on random spawn

--- blipdot/app/src/main/java/com/joshi/blipdot/MainActivity.kt
+++ blipdot/app/src/main/java/com/joshi/blipdot/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.joshi.blipdot
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.style.AbsoluteSizeSpan
+import android.util.DisplayMetrics
+import android.view.Display
+import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.AbsoluteLayout
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +24,36 @@ class MainActivity : AppCompatActivity() {
         return Random(System.nanoTime()).nextInt(end - start + 1) + start
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val constLayout = findViewById<ConstraintLayout>(R.id.blackBackground)
 
-        val yellowBtn = findViewById<Button>(R.id.yellowBtn)
-        val blackBackground = findViewById<AbsoluteLayout>(R.id.blackBackground)
+        // creating the button
+        val yellowBtn = Button(this)
+        // setting layout_width and layout_height using layout parameters
+        yellowBtn.layoutParams = ConstraintLayout.LayoutParams(45, 45)
+        yellowBtn.text = ""
+        yellowBtn.setBackgroundResource(R.drawable.roundedbutton)
+        yellowBtn.alpha = 1F
+        yellowBtn.textSize = 15F
 
-        blackBackground.setOnClickListener() {
+        // add Button to LinearLayout
+        constLayout.addView(yellowBtn)
+
+        constLayout.setOnClickListener {
             Toast.makeText(this, "You Lost", Toast.LENGTH_SHORT).show()
         }
 
-        yellowBtn.setOnClickListener() {
-            val animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
-            yellowBtn.startAnimation(animation)
+        yellowBtn.setOnClickListener {
+            val animationOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            yellowBtn.startAnimation(animationOut)
+            val animationIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            yellowBtn.translationX = rand(0, 50).toFloat()
+            yellowBtn.translationY = rand(0, 50).toFloat()
+            yellowBtn.startAnimation(animationIn)
+
         }
     }
 }
