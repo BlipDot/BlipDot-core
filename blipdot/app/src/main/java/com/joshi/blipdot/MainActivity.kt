3bba29d782293dadb0dd33c36a7e5492085640e8
Creatune <rjjoshixyz@gmail.com>
2020-05-08 14:17:58 +0530

    Added random number function

--- blipdot/app/src/main/java/com/joshi/blipdot/MainActivity.kt
+++ blipdot/app/src/main/java/com/joshi/blipdot/MainActivity.kt
@@ -2,15 +2,31 @@ package com.joshi.blipdot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AbsoluteLayout
 import android.widget.Button
+import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    fun rand(start: Int, end: Int): Int {
+        require(!(start > end || end - start + 1 > Int.MAX_VALUE)) { "Illegal Argument" }
+        return Random(System.nanoTime()).nextInt(end - start + 1) + start
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var yellowBtn = findViewById<Button>(R.id.yellowBtn)
+        var blackBackground = findViewById<AbsoluteLayout>(R.id.blackBackground)
 
+        blackBackground.setOnClickListener() {
+            Toast.makeText(this, "You Lost", Toast.LENGTH_SHORT).show()
+        }
+
+        yellowBtn.setOnClickListener() {
+
+        }
     }
 }
