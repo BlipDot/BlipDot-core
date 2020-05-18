f8ea2bc4a9fb03cdd979f36b56d384dcbf99eba5
Creatune <rjjoshixyz@gmail.com>
2020-05-18 16:16:53 +0530

    Changed startup page

---
+++ blipdot/app/src/main/java/com/joshi/blipdot/StartupActivity.kt
@@ -0,0 +1,11 @@
+package com.joshi.blipdot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.startupdark)
+    }
+}
