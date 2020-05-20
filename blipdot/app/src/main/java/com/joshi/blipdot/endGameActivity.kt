5e6aab3efa87f5da8931ca1b713039429cbac130
Creatune <rjjoshixyz@gmail.com>
2020-05-20 11:31:05 +0530

    Removed top and bottom nav bars in end game activity

--- blipdot/app/src/main/java/com/joshi/blipdot/endGameActivity.kt
+++ blipdot/app/src/main/java/com/joshi/blipdot/endGameActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class endGameActivity:AppCompatActivity() {
     private var mediaPlayer: MediaPlayer? = null
@@ -31,6 +33,12 @@ class endGameActivity:AppCompatActivity() {
 
         var score = intent.getIntExtra("score", 0)
         scoreDisplay.text = "Score: $score"
+
+        val background = findViewById<ConstraintLayout>(R.id.background)
+
+        background.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
         homeBtn.setOnClickListener() {
             popSoundEffect(homeBtn, sound = R.raw.pop_sound)
