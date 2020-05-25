38e4c5ef8712673428aca0cb7ffa78a2874e3cd6
Creatune <rjjoshixyz@gmail.com>
2020-05-26 00:06:02 +0530

    Fixed yellow dot error

--- blipdot/app/src/main/java/com/joshi/blipdot/intermediateActivity.kt
+++ blipdot/app/src/main/java/com/joshi/blipdot/intermediateActivity.kt
@@ -1,7 +1,6 @@
 package com.joshi.blipdot
 
 import android.content.Intent
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -14,18 +13,17 @@ class intermediateActivity:AppCompatActivity() {
     lateinit var countdown_timer: CountDownTimer
     var isRunning: Boolean = false
     var time_in_milli_seconds: Long = 0L
-    private var mediaPlayer: MediaPlayer? = null
 
     private fun startTimer(time_in_seconds: Long) {
         countdown_timer = object : CountDownTimer(time_in_seconds, 1000) {
             override fun onFinish() {
-                var intent = Intent(this@intermediateActivity, MainActivity::class.java)
-                startActivity(intent)
+                var intent1 = Intent(this@intermediateActivity, MainActivity::class.java)
+                startActivity(intent1)
             }
             override fun onTick(p0: Long) {
                 time_in_milli_seconds = p0
                 val countDownText = findViewById<TextView>(R.id.countDownText)
-                time_in_milli_seconds /= 1000L
+                time_in_milli_seconds = (time_in_milli_seconds/1000L) + 1L
                 countDownText.text = time_in_milli_seconds.toString()
             }
         }
@@ -33,32 +31,15 @@ class intermediateActivity:AppCompatActivity() {
         isRunning = true
     }
 
-    private fun popSoundEffect(button: Button, sound: Int) {
-        if (mediaPlayer != null && mediaPlayer!!.isPlaying) {
-            mediaPlayer!!.stop()
-            mediaPlayer!!.reset()
-            mediaPlayer = MediaPlayer.create(this, R.raw.pop_sound)
-            mediaPlayer?.start()
-        } else {
-            mediaPlayer = MediaPlayer.create(this, R.raw.pop_sound)
-            mediaPlayer?.start()
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.intermediate)
-        val skipBtn = findViewById<Button>(R.id.skipBtn)
         val layout = findViewById<ConstraintLayout>(R.id.background)
 
         layout.systemUiVisibility = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
-        startTimer(5000)
-        skipBtn.setOnClickListener() {
-            popSoundEffect(skipBtn, sound = R.raw.pop_sound)
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+        startTimer(3000)
     }
 }
